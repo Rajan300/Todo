7b8ff0a0d202dab6af409cf0700b4b51d3a5515b
Rajan300 <rajankc1002@gmail.com>
2024-03-03 13:54:46 -0600

    Secondcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
+import { useState,useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [todoList, setTodoList] = useState([])
+  const[currentTask,setCurrentTask] = useState("")
+  const inputTask=useRef(null);
 
+  const addTask=()=>{
+    setTodoList([...todoList,currentTask])
+     inputTask.current.value=""
+     setCurrentTask("")
+   
 
+  }
   return (
     <div className="App">
       <h1>Todo List</h1>
+    <div>
+    <input ref={inputTask} type="text" placeholder="Task" onChange={(event)=>{setCurrentTask(event.target.value)}}/>
+
+    <button onClick={addTask}>AddTask</button>
+    </div>
+    <hr />
+    <ul>
+     {todoList.map((value,key)=>{
+      return (
+        <li key={key}>{value}</li>
+      )
+     })}
+    </ul>
     </div>
   )
 }
